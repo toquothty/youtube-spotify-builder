26c7099f45511e8f63ea5a36d93ce0e8499c9755
Thomas Toquothty <tltoquothty@gmail.com>
2021-08-31 15:06:58 -0400

    Update gitignore, start creating script hierarchy

---
+++ playlist-builder.py
@@ -0,0 +1,18 @@
+# The intent of this program is to grab videos from my youtube music
+# playlist and then add them to a spotify playlist named TQ-Youtube.
+# author: Thomas Toquothty, 2021
+
+# Step 1: Login to youtube
+def youtube_connect():
+
+# Step 2: Get playlist "Music"
+def grab_youtube_list():
+
+# Step 3: Create spotify playlist, TQ-Youtube
+def create_playlist():
+
+# Step 4: Search spotify for song
+def search_spotify():
+
+# Step 5: Add the song to TQ-Youtube.
+def add_song_playlist():
